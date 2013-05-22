dace030f259a2753129c2ebe74676411f2c627bc
igorcb <igor.batista@gmail.com>
2013-05-22 15:55:26 -0300

    validacao no model cliente

--- app/models/cliente.rb
+++ app/models/cliente.rb
@@ -1,3 +1,5 @@
 class Cliente < ActiveRecord::Base
   attr_accessible :celular, :email, :endereco, :nome, :numero, :telefone
+  validates :celular, :email, :endereco, :nome, :numero, :telefone, presence: :true
+
 end
